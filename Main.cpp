a9723e4ec33e6f23415de84302a830475f088582
Salma269 <127438269+Salma269@users.noreply.github.com>
2023-12-15 14:06:16 +0200

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -4,64 +4,78 @@ using namespace std;
 int main(){
     cout<<"Hello, Welcome to Contact list"<<endl;
     ContactList myContacts;
-    string name, number;
+    string name, number, email, address;
     int choice;
+
+    // Load contacts from binary file
+    ifstream inFile("contacts.bin", ios::binary);
+    if (inFile) {
+        myContacts.loadFromBinaryFile(inFile);
+        inFile.close();
+    }
 
     while (true) {
         cout << "Please choose an operation:\n";
         cout << "1. Insert new contact\n";
         cout << "2. Search for contact by name\n";
         cout << "3. Display contactList\n";
-        cout<<  "4. Display information of contact"
+        cout << "4. Display information of contact\n";
         cout << "5. Delete contact\n";
-        cout << "6. Delete all contacts\n";
+        cout << "6. Edit contact information\n";
         cout << "7. Exit\n";
         cin >> choice;
+        cin.ignore();  // Ignore newline character in input buffer
 
         switch (choice) {
             case 1:
                 cout << "Enter contact name: ";
-                cin >> name;
+                getline(cin, name);
                 cout << "Enter contact number: ";
-                cin >> number;
-                cout << "Enter contact email ";
-                cin >> email;
+                getline(cin, number);
+                if(!myContacts.isValidPhoneNumber(number)){
+                    cout<<"wrong number format"<<endl;
+                    break;
+                }
+               
+                cout << "Enter contact email: ";
+                getline(cin, email);
                 cout << "Enter contact address: ";
-                cin >> address;
-                myContacts.insert(name, number,email,address);
+                getline(cin, address);
+                Contact h(name, number, email, address);
+                myContacts.insert(h);
                 break;
+        
             case 2:
                 cout << "Enter contact name to search: ";
-                cin >> name;
-                Contact* foundcontact = myContacts.search(name);
-                if (foundContact != nullptr) {
-                    cout << "Contact found: " << foundContact->getName() << ", " << foundContact->getNumber() << endl;
-                } else {
-                    cout << "Contact not found." << endl;
-                }
+                getline(cin, name);
+                myContacts.Search(cout, name);
                 break;
             case 3:
-                myContacts.display();
+                myContacts.display(cout);
                 break;
             case 4:
                 cout << "Enter contact name to display information: ";
-                cin >> name;
-                Contact* foundcontact = myContacts.search(name);
-                if (foundContact != nullptr) {
-                    cout << "Contact found: " << foundContact->getName() << ", number:  " << foundContact->getNumber() <<<< ", email: " << foundContact->getEmail() <<<< ", address: " << foundContact->getAddress() << endl;
-                } else {
-                    cout << "Contact not found." << endl;
-                }
+                getline(cin, name);
+                myContacts.Search(cout, name);
                 break;
             case 5:
                 cout << "Enter contact name to delete: ";
-                cin >> name;
-                myContacts.deleteContact(name);
+                getline(cin, name);
+                myContacts.deleteContact1(cout, name);
+                 if (!myContacts.isValidSize()) {
+                 cout << "Error:: invalid list size" << endl;
+                }
                 break;
             case 6:
-                myContacts.deleteContactList();
+                cout << "Enter contact name to edit: ";
+                getline(cin, name);
+                myContacts.editContact1(cout, name);
                 break;
             case 7:
+                // Save contacts to binary file before exiting
+                ofstream outFile("contacts.bin", ios::binary);
+                myContacts.saveToBinaryFile(outFile);
+                outFile.close();
                 return 0;
             default:
                 cout << "Invalid choice. Please try again.\n";
@@ -69,4 +83,4 @@ int main(){
     }
 
     return 0;
-};
+}
